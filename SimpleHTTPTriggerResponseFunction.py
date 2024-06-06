d7ab607b0ffae7be7883e684263bb93b8c7d8fc8
Maziar Sojoudian <maziar@sojoudian.net>
2024-06-06 15:21:41 -0400

    add SimpleHTTPTriggerResponseFunction.py

---
+++ SimpleHTTPTriggerResponseFunction.py
@@ -0,0 +1,12 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="http_trigger1")
+def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # Return a simple message stating the API is working fine
+    return func.HttpResponse("API working fine", status_code=200)
+
